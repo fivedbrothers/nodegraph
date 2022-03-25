c55eadbf5d281d8fdd3e72a182be25820acd28e4
Mark Dasco <mrkrynmdsco@gmail.com>
2022-03-25 09:04:40 +0800

    Initial commit :: NodeGraphQt translated via QtPy

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # qtpy-nodes
 Node Graph Framework in QtPy
+
+
+
+
+## References
+[NodeGraphQt](https://github.com/jchanvfx/NodeGraphQt) by Johnny Chan
+
+[PyQtNodeEditor](https://gitlab.com/pavel.krupala/pyqt-node-editor) by Pavel Křupala
+
+[QtPy](https://github.com/spyder-ide/qtpy) by Spyder-IDE
+
+[Qt for Python](https://doc.qt.io/qtforpython/) by Qt Company
+
---
+++ setup.py
@@ -0,0 +1,52 @@
+
+import os
+import sys
+
+import setuptools
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'nodegraphqtpy'))
+
+import pkg_info
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+description = (
+    'Node graph framework that can be re-implemented into applications that '
+    'supports PySide, PySide2 & PySide6 via QtPy'
+)
+classifiers = [
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.9',
+    # 'Documentation :: https://jchanvfx.github.io/NodeGraphQt/api/html/index.html',
+    # 'Source :: https://github.com/jchanvfx/NodeGraphQt/',
+]
+
+setuptools.setup(
+    name=pkg_info.__module_name__,
+    version=pkg_info.__version__,
+    author=pkg_info.__author__,
+    author_email='',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=pkg_info.__url__,
+    packages=setuptools.find_packages(exclude=['example_nodes']),
+    classifiers=classifiers,
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires='>=3.9',
+    extras_require={
+        'PySide6': ['PySide6>=6.2']
+    }
+)
+
+
+"""
+python setup.py sdist
+sudo python setup.py install
+"""
